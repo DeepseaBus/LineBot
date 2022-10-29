a23d40cb34159b044cf104c53ef6ff2cc3eb8a98
Tim.Chang <tim3553431992@hotmail.com.tw>
2022-10-30 01:29:11 +0800

    update image_processing.py find ROI(region of interest) with color

--- linebotapp/image_processing.py
+++ linebotapp/image_processing.py
@@ -10,7 +10,7 @@ def image_processing_1(image_name, image_path):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # convert to binary
-    ret, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+    ret, binary = cv.threshold(gray, 200, 255, cv.THRESH_BINARY)
 
     # save converted image in path
     gray_path = './static/gray_' + image_name
@@ -20,7 +20,8 @@ def image_processing_1(image_name, image_path):
 
     # put binary image in contours
     contours, hierarchy = cv.findContours(binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
+    # copy img
+    copy = img.copy()
     # create empty img
     empty = np.ones(img.shape, dtype=np.uint8) * 255
     # find ROI(region of interest)
@@ -31,23 +32,35 @@ def image_processing_1(image_name, image_path):
     for i in range(n):
         M = cv.moments(contours[i])
         area = M['m00']
-        if area / total_area > 0.15 and area / total_area < 0.50:  # if coutour area > 15% total area <50%
+        if area / total_area > 0.15 and area / total_area < 0.50:  # if contour area > 15% total area <50%
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
             center = tuple((cx, cy))
             if cx > img.shape[1] * 0.25 and cx < img.shape[1] * 0.75:
                 # put text on empty img
-                cv.putText(empty, '. no.=' + str(i), center, font, 2, (0, 0, 255), 1)
-                print('輪廓編號%d，面積大小%d，中心點%s' % (i, area, str(center)))
-                # draw coutour on copy img
-                copy = cv.drawContours(empty, contours[i], -1, (255, 0, 0), 2)
+                # cv.putText(empty, '. no.=' + str(i), center, font, 2, (0, 0, 255), 1)
+                # print('輪廓編號%d，面積大小%d，中心點%s' % (i, area, str(center)))
+                # draw contour on copy img
+                # copy = cv.drawContours(empty, contours[i], -1, (255, 0, 0), 2)
+
+                # draw contour on copy img
+                # copy = cv.drawContours(img, contours[i], -1, (255, 0, 0), 2)
+
+                # bulid single channel mask
+                mask = np.zeros(gray.shape, dtype=np.uint8)
+                cv.drawContours(mask, contours[i], -1, (255, 255, 255), -1)
+                meanVal = cv.mean(img, mask=mask)  # mask is an area, so it has to be single channel
+                if meanVal[2]>meanVal[1] and meanVal[2]>meanVal[1]:
+                    print(meanVal)  # print contour's B、G、R average
+                    if abs(meanVal[2] - meanVal[1]) > 10:  # if abs(green - red)>10
+                        copy = cv.drawContours(img, contours[i], -1, (0, 0, 255), 2)
     # copy origin img
     # copy = img.copy()
 
     # sketch contour on img
     # copy = cv.drawContours(copy, contours, -1, (255, 0, 0), 2)
-    copy = cv.drawContours(empty, contours, -1, (255, 0, 0), 2)
+    # copy = cv.drawContours(empty, contours, -1, (255, 0, 0), 2)
     # save after contoured img
-    contour_image_path = './static/contour_'+ image_name
+    contour_image_path = './static/contour_' + image_name
     cv.imwrite(contour_image_path, copy)
     return gray_path, binary_path, contour_image_path
